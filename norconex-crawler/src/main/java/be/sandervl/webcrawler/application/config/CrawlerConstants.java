70fe46f72426081095fec5a2d252bc6a351df35e
svl@foreach.be <svl@foreach.be>
2018-12-27 21:19:18 +0100

    constants

--- norconex-crawler/src/main/java/be/sandervl/webcrawler/application/config/CrawlerConstants.java
+++ norconex-crawler/src/main/java/be/sandervl/webcrawler/application/config/CrawlerConstants.java
@@ -4,13 +4,13 @@ public interface CrawlerConstants {
     String ELASTICSEARCH_URL = "http://localhost:9200";
 
     int MAX_DEPTH = 2;
-    int MAX_DOCUMENTS = 50;
-    int COMMIT_BATCH_SIZE = 1;
+    int MAX_DOCUMENTS = 250;
+    int COMMIT_BATCH_SIZE = 10;
 
     String TARGET_CONTENT_FIELD = "body";
 
-    String WORK_DIRECTORY = "../data/crawler/work";
     String QUEUE_DIRECTORY = "../data/committer";
+    String WORK_DIRECTORY = "../data/crawler/work";
     String LOGS_DIRECTORY = "../data/crawler/logs";
     String PROGRESS_DIRECTORY = "../data/crawler/progress";
 }
