23ae68965f4462cd7e22ea3a2a7f46b0bfde2d9e
sandervanloock <sandervl34@gmail.com>
2022-02-01 20:57:05 +0100

    preview styling

---
+++ frontend/src/app/components/crawl-preview/crawl-preview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crawl-preview',
+  templateUrl: './crawl-preview.component.html',
+  styleUrls: ['./crawl-preview.component.scss']
+})
+export class CrawlPreviewComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
